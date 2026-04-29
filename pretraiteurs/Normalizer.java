af80ccf305ea2ea240f3cfdcd85f052f5c95ede2
ilyesTxc <ilyes.boulifi@etudiant-enit.utm.tn>
2026-04-29 19:54:13 +0100

    testing normalizer preprocessing

--- pretraiteurs/Normalizer.java
+++ pretraiteurs/Normalizer.java
@@ -2,11 +2,35 @@ package kyc.pretraiteurs;
 
 import kyc.pretraiteurs.PreTraiteurNom;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Normalizer implements PreTraiteurNom {
-    @Override
     public List<String> preTraiter(List<String> noms) {
-        // TODO : normaliser chaque nom (trim, espaces multiples, etc.)
-        return null;
+        List<String> results = new ArrayList<>();
+
+        for(String nom : noms){
+                nom = nom.trim();
+                nom = nom.replaceAll("\\s+"," ");
+                results.add(nom);
+        }
+
+        return results;
     }
+
+    public static void main(String[] args) {
+        PreTraiteurNom normalizer = new Normalizer();
+
+        List<String> noms = Arrays.asList(
+                "  Ahmed    Ben   Ali  ",
+                "  ILYÈS  ",
+                "Sâmi     Salah",
+                "   Mohamed\tAli   "
+        );
+
+        List<String> resultats = normalizer.preTraiter(noms);
+
+        System.out.println(resultats);
+    }
+
 }
