0930a2bd57ac2a19272a3d85232809e907253d51
isra314 <isra.maaref@etudiant-enit.utm.tn>
2026-04-29 23:02:01 +0100

    Fix arrow syntax in AccentRemover

--- pretraiteurs/AccentRemover.java
+++ pretraiteurs/AccentRemover.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 public class AccentRemover implements PreTraiteurNom {
 
     public List<String> preTraiter(List<String> noms) {
-        return noms.stream().map(noms → Normalizer.normalize(noms, Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}", "")).collect(Collectors.toList());
+        return noms.stream().map(nom -> Normalizer.normalize(nom, Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}", "")).collect(Collectors.toList());
 
     }
 }
