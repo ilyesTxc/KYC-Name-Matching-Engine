acf78b6605c6ef0f8c1e0b2d51de96a9e4d9cc75
Emna <emna.marzougui@etudiant-enit.utm.tn>
2026-05-04 00:06:21 +0100

    Add ParSeuil class

--- selectionneurs/ParSeuil.java
+++ selectionneurs/ParSeuil.java
@@ -3,7 +3,9 @@ package kyc.selectionneurs;
 import kyc.selectionneurs.SelectionMatching;
 import kyc.model.Nom;
 import kyc.model.CoupleValeur;
+import kyc.model.Resulat;
 import java.util.List;
+import java.util.ArrayList;
 
 public class ParSeuil implements SelectionMatching {
     private double seuil;
@@ -12,9 +14,17 @@ public class ParSeuil implements SelectionMatching {
         this.seuil = seuil;
     }
 
-    @Override
-    public List<Nom> selectionner(List<CoupleValeur> couples) {
-        // TODO : retourner les noms dont le score >= seuil
-        return null;
+    public List<Resultat> selectionner(List<CoupleValeur> couples) {
+        if (couples == null){
+            return new ArrayList<>();
+        }
+        List<Resultat> resultats= new ArrayList<>();
+        for (CoupleValeur couple: couples){
+            if ( couple != null && couple.getScore() >= seuil){
+                Resultat resultat = new Resultat( nomClient, couple.getNom(), couple.getScore());
+            }
+            resultats.add(resultat);
+        }
+        return resultats;
     }
 }
