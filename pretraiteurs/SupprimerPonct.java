84d847562cfae8752bd1ac6db56e396b86f77c07
Emna <emna.marzougui@etudiant-enit.utm.tn>
2026-05-03 23:11:49 +0100

    Add SupprimerPonct class

--- pretraiteurs/SupprimerPonct.java
+++ pretraiteurs/SupprimerPonct.java
@@ -1,12 +1,21 @@
 package kyc.pretraiteurs;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SupprimerPonct implements PreTraiteurNom{
     public List<String> preTraiter(List<String> noms){
-        List<String> resultat= noms.stream()
-                .map(n-> n.replaceAll("[^\\p{L}\\p{N}\\s]",""))
-                .collect(Collectors.toList());
+        if(noms== null){
+            return new ArrayList<>();
+        }
+        List<String> results= new ArrayList<>();
+        for(String nom: noms){
+            if(nom==null){
+                continue;
+            }
+            nom=nom.replaceAll("[^\\p{L}\\p{N}\\s]","");
+            results.add(nom);
+        }
         return resultat;
     }
 }
