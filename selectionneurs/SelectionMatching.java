d9b244476b37355f889c58700975b3548ac5a8ea
isra314 <isra.maaref@etudiant-enit.utm.tn>
2026-05-04 11:43:30 +0100

    Update SelectionMatching

--- selectionneurs/SelectionMatching.java
+++ selectionneurs/SelectionMatching.java
@@ -2,8 +2,10 @@ package kyc.selectionneurs;
 
 import kyc.model.Nom;
 import kyc.model.CoupleValeur;
+import kyc.model.Resultat;
+
 import java.util.List;
 
 public interface SelectionMatching {
-    List<Nom> selectionner(List<CoupleValeur> couples);
+    List<Resultat> selectionner(Nom nomClient, List<CoupleValeur> couples);
 }
