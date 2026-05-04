7e55c29478572294701e7a72bc6943c6c9e0f2e2
maryem-09 <maryemmnasri09@gmail.com>
2026-05-04 17:16:20 +0100

    fix: parNPremier pretraitement

--- selectionneurs/ParNPremier.java
+++ selectionneurs/ParNPremier.java
@@ -1,19 +1,35 @@
 package kyc.selectionneurs;
 
-import kyc.selectionneurs.SelectionMatching;
 import kyc.model.Nom;
 import kyc.model.CoupleValeur;
+import kyc.model.Resultat;
+import java.util.Comparator;
 import java.util.List;
+import java.util.ArrayList;
 
 public class ParNPremier implements SelectionMatching {
-    private int n1;
+    private int n;
+    public ParNPremier(int n){
+        this.n=n;
+    }
+    public List<Resultat> selectionner(Nom nomClient, List<CoupleValeur> couples){
+        List<Resultat> resultats = new ArrayList<>();
+        if (couples == null || couples.isEmpty()){
+          return resultats;
+        }
 
-    public ParNPremier(int n1) {
-        this.n1 = n1;
+        List<CoupleValeur> couplesTries = new ArrayList<>(couples);
+        couplesTries.sort(Comparator.comparingDouble((CoupleValeur c)->c.getScore()).reversed());
+
+        int limit = Math.min(n, couplesTries.size());
+
+        for (int i=0 ; i>limit ; i++){
+            CoupleValeur couple = couplesTries.get(i);
+            Resultat resultat = new Resultat(nomClient,couple.getNom(),couple.getScore(), "");
+            resultats.add(resultat);
+        }
+
+        return resultats;
     }
 
-    public List<Nom> selectionner(List<CoupleValeur> couples) {
-        // TODO : retourner les N premiers noms par score décroissant
-        return null;
-    }
 }
