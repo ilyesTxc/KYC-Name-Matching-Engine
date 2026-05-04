47a9c6be65622773dc43ef9edf8ba0ed04780de1
Emna <emna.marzougui@etudiant-enit.utm.tn>
2026-05-04 09:30:46 +0100

    Add ParSeuil class

--- selectionneurs/SelectionMatching.java
+++ selectionneurs/SelectionMatching.java
@@ -4,6 +4,7 @@ import kyc.model.Nom;
 import kyc.model.CoupleValeur;
 import kyc.model.Resultat;
 import java.util.List;
+import java.util.ArrayList;
 
 public interface SelectionMatching {
     List<Resultat> selectionner(List<CoupleValeur> couples);
