496a445d45fda172f11d46608695346dae4ca662
isra314 <isra.maaref@etudiant-enit.utm.tn>
2026-05-08 13:38:49 +0100

    update GenerateurCandidat and ListManager

--- app/GenerateurCandidat.java
+++ app/GenerateurCandidat.java
@@ -5,5 +5,5 @@ import java.util.List;
 import java.util.Map;
 
 public interface GenerateurCandidat {
-    Map<Nom, List<Nom>> genererCandidats(List<Nom> nomClient, List<Nom> listSanctionnes);
+    Map<Nom, List<Nom>> genererCandidats(List<Nom> listeGauche, List<Nom> listeDroite);
 }
