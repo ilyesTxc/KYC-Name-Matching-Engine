393f3b007ac7289f0f9e81b31dcb695e7a689e68
maryem-09 <maryemmnasri09@gmail.com>
2026-05-09 13:30:35 +0100

    fix: added Fichier source to ParNPremier and Nom

--- model/Nom.java
+++ model/Nom.java
@@ -5,6 +5,7 @@ public class Nom {
     private int id;
     private String nomOriginal;
     private List<String> nomPretraite;
+    private String fichierSource;
 
     public Nom(int id, String nomOriginal) {
         this.id = id;
@@ -15,6 +16,8 @@ public class Nom {
     public String getNomOriginal() { return nomOriginal; }
     public List<String> getNomPretraite() { return nomPretraite; }
     public void setNomPretraite(List<String> nomPretraite) { this.nomPretraite = nomPretraite; }
+    public String getFichierSource() {return fichierSource; }
+    public void setFichierSource(String f) { this.fichierSource = f; }
     public String toString() {
         return "Nom{id=" + id +", nomOriginal='" + nomOriginal+"'}";
     }
