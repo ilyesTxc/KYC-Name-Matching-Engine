085d37960a570acf1b75e145c8376939d08f7303
maryem-09 <maryemmnasri09@gmail.com>
2026-05-09 14:46:38 +0100

    fix LowerCase

--- pretraiteurs/LowerCase.java
+++ pretraiteurs/LowerCase.java
@@ -5,16 +5,16 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class LowerCase implements PreTraiteurNom {
-    public List<String> preTraiter(List<String> noms){
-        if(noms== null){
+    public List<String> preTraiter(List<String> noms) {
+        if (noms == null) {
             return new ArrayList<>();
         }
-        List<String> results= new ArrayList<>();
-        for(String nom: noms){
-            if(nom==null){
+        List<String> results = new ArrayList<>();
+        for (String nom : noms) {
+            if (nom == null) {
                 continue;
             }
-            nom= nom.toLowerCase();
+            nom = nom.toLowerCase();
             results.add(nom);
         }
         return results;
@@ -22,29 +22,4 @@ public class LowerCase implements PreTraiteurNom {
 
     }
 
-    public static class NGram implements PreTraiteurNom{
-        private int n;
-        public NGram(){
-            this.n = 2;
-        }
-        public NGram(int n){
-            this.n = n;
-        }
-        public List<String> preTraiter(List<String> noms){
-            List<String> result = new ArrayList<>();
-            for (String nom : noms) {
-                result.addAll(extraireNGrams(nom));
-            }
-            return result;
-
-        }
-        private List<String> extraireNGrams(String s){
-            if (s == null || s.length() < n) return Collections.emptyList();
-            List<String> ngrams = new ArrayList<>();
-            for ( int i = 0; i <= s.length() - n; i++) {
-                ngrams.add(s.substring(i, i + n));
-            }
-            return ngrams;
-        }
-    }
 }
